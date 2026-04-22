d1ccc7309ae401391a6386c31a266ae1f3a8215b
alessan2ro <rguezalessandro05@gmail.com>
2026-04-22 06:07:28 -0400

    cambio de nombre en el inicio

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -90,7 +90,7 @@ export default function Navbar() {
               height={32}
               priority
             />
-            LoTengo
+            Mercacentro
           </Link>
 
           {/* SearchBar escritorio */}
